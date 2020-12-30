5230702ad7d1f6a914a658c1a660ae64c0015a28
Maxence <lemairemaxence@hotmail.com>
2020-12-30 15:15:29 +0100

    exo ok

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+let array = ["un", "deux", "trois"]
+console.log(array);
+
+array.push('maxence')
+console.log(array);
+
+array.splice(1, 1, "bis")
+console.log(array);
+
+array.splice(2, 1)
+console.log(array);
